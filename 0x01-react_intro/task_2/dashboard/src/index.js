87262ff15f00cafb8517107d1b4053c44a3a8c98
harith01 <harithyusuph@gmail.com>
2023-10-26 11:15:16 +0100

    Task 4

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -4,11 +4,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Notifications from './Notifications';
 
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Notifications />
   </React.StrictMode>
 );
 

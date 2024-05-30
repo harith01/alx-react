95141467e43150fae83e3b1001a81378a485bf59
harith01 <harithyusuph@gmail.com>
2024-05-30 17:48:34 +0100

    Task 1

---
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import './App.css';
+import PropTypes from "prop-types";
+import CourseList from "../CourseList/CourseList";
+import { getLatestNotification } from "../utils/utils";
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: getLatestNotification() },
+];
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    if (event.ctrlKey && event.key === 'h') {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Notifications listNotifications={listNotifications}/>
+        <div className="App">
+          <div className="App-header">
+            <Header />
+          </div>
+          <div className="App-body">
+            {this.props.isLoggedIn ? <CourseList listCourses={listCourses}/> : <Login />}
+          </div>
+          <div className="App-footer">
+            <Footer />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => { }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+}
+
+
+export default App;
